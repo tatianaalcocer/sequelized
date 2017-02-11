b380730d28b7d87b9d4650554ba01b7aa578e5a7
Al Min <albertmin@Lilu.local>
2017-02-10 23:43:08 -0500

    Start, pause, and restart functionality

--- public/js/init.js
+++ public/js/init.js
@@ -1,14 +1,33 @@
 (function($) {
   $(function() {
     var counter = 25;
-  var timer = moment.duration(1, "seconds").timer({loop: true},function() {
+    $('#time').text(counter);
+  var timer = moment.duration(1, "seconds").timer({
+    loop: true,
+    start: false
+  },function() {
     counter--;
     $('#time').text(counter);
     if(counter === 0) {
       timer.stop();
     }
   });
-
+  $('#start-button').on('click', function() {
+    if($('#start-button').text() === 'Start') {
+      $('#start-button').text('Pause');
+    } else {
+      $('#start-button').text('Start');
+    }
+    if(timer.isStopped() || !timer.started) {
+      timer.start();
+    } else {    
+      timer.stop();
+    }
+  });
+  $('#restart-button').on('click', function() {
+    counter = 25;
+    $('#time').text(counter);
+  });
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
