5e07ce8accf6d7ae99f08a27fff4296c6a282305
Al Min <albertmin@Lilu.local>
2017-02-11 19:43:53 -0500

    Redone with TimeCircles library

--- public/js/init.js
+++ public/js/init.js
@@ -1,32 +1,57 @@
 (function($) {
   $(function() {
-    var counter = 25;
-    $('#time').text(counter);
-  var timer = moment.duration(1, "seconds").timer({
-    loop: true,
-    start: false
-  },function() {
-    counter--;
-    $('#time').text(counter);
-    if(counter === 0) {
-      timer.stop();
-    }
-  });
+  $("#stopwatch").TimeCircles({
+    time: {
+      Days: {show: false},
+      Hours: {show: false}
+    },
+    // start: false 
+  });    
+  $('#stopwatch').TimeCircles().restart();
+  $('#stopwatch').TimeCircles().stop();
+    // var counter = 1500000;
+    // $('#time').text(moment(counter).format("mm:ss.SSS"));
+  // var timer = moment.duration(1, "milliseconds").timer({
+  //   loop: true,
+  //   start: false
+  // },function() {
+  //   counter--;
+  //   $('#time').text(moment(counter).format("mm:ss.SSS"));
+  //   if(counter === 0) {
+  //     timer.stop();
+  //   }
+  // });
+  var started = false;
   $('#start-button').on('click', function() {
-    if($('#start-button').text() === 'Start') {
+    if(!started) {
       $('#start-button').text('Pause');
+      $('#stopwatch').TimeCircles().start();
+      started = true; 
     } else {
       $('#start-button').text('Start');
+      $('#stopwatch').TimeCircles().stop();
+      started = false;
     }
-    if(timer.isStopped() || !timer.started) {
-      timer.start();
-    } else {    
-      timer.stop();
-    }
+    // if($('#start-button').text() === 'Start') {
+    //   $('#start-button').text('Pause');
+    // } else {
+    //   $('#start-button').text('Start');
+    // }
+    // if(timer.isStopped() || !timer.started) {
+    //   timer.start();
+    // } else {    
+    //   timer.stop();
+    // }
   });
   $('#restart-button').on('click', function() {
-    counter = 25;
-    $('#time').text(counter);
+    $('#stopwatch').TimeCircles().restart();
+    $('#stopwatch').TimeCircles().stop();
+    if(started) {
+      $('#start-button').text('Start');      
+      started = false;
+    }
+    // counter = 1500000;
+    // $('#time').text(moment(counter).format("mm:ss.SSS"));
   });
 
   }); // end of document ready
